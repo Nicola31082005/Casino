7f7caee6ca40960c4edfde34b06c0fc689479a0f
Nicola31082005 <nikule512@gmail.com>
2024-12-26 00:29:10 +0200

    Fixed project structure

---
+++ casino-game/tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    './index.html', // Include this if you're using the index.html directly
+    "./public/**/*.{html,js}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
